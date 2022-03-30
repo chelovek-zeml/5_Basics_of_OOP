66896ba2c0063ba04e605d25d3319fc0edc4d716
chelovek-zeml <stranger37561@gmail.com>
2022-03-30 13:08:46 +0300

    add mod05 ver2

--- mod05/a05/Task05.java
+++ mod05/a05/Task05.java
@@ -1,5 +1,4 @@
 package com.epam.jonline.mod05.a05;
 
 public class Task05 {
-
 }
---
+++ mod05/a05/bean/Package.java
@@ -0,0 +1,14 @@
+package com.epam.jonline.mod05.a05.bean;
+
+import java.io.Serializable;
+
+public class Package implements Serializable {
+
+    private static final long serialVersionUID = 4L;
+
+    private String material;
+    private double price;
+
+
+
+}
